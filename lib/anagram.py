ecf72bb7ab481ea90ff62c573357224e497173da
+Sam Thongo <+samuelthongo73@gmail.com>
2023-10-17 15:28:52 +0300

    anagram

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,16 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, words):
+        anagrams = [word for word in words if sorted(words) == sorted(self.word)]
+        if len(anagrams) > 0:
+            return anagrams
+        else:
+            return []
+            
+
+listen = Anagram(['listen'])
+anagrams = listen.match(['enlists', 'google', 'inlets', 'banana'])
+print(anagrams)
+pass
